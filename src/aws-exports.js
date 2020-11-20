c0acc5d52575bf795e4ca7672c3340185ddd4f83
togawa <61341108+sakanafuto@users.noreply.github.com>
2020-11-20 10:39:22 +0900

    GlaphQL APIの追加

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "ap-northeast-1",
     "aws_user_pools_id": "ap-northeast-1_MMImrRwew",
     "aws_user_pools_web_client_id": "rpmbf5jhjscr79200ni4grsnn",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://he7ophgghvfq5fz7zne6kjbugu.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-northeast-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-of5r7d3aivepxgs356z4l22oha"
 };
 
 
